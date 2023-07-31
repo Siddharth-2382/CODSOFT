d000f90ef0100aac707700ad36ba1b75c35e7281
Siddharth Movaliya <siddraj1992@gmail.com>
2023-07-31 23:16:54 +0530

    add meaningful comments and replace Entry with Spinbox

--- Password-Generator/main.py
+++ Password-Generator/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 # Define constants
 RED = "#e7305b"
@@ -16,31 +17,41 @@ def generate_password():
 # GUI using tkinter
 window = Tk()
 
+# Label for the title
 title_label = Label(text="Welcome To Password Generator!", bg=YELLOW, fg=PURPLE, font=(FONT_NAME, 45))
 title_label.pack()
 
-letter_label = Label(text="1. How many letters would you like in your password?", bg=YELLOW, fg=BLACK, font=(FONT_NAME, 20))
+# Label for Number of Letters
+letter_label = Label(text="1. How many letters would you like in your password?", bg=YELLOW, fg=BLACK, font=(FONT_NAME,
+                                                                                                             20))
 letter_label.place(x=10, y=100)
 
-letter_input = Entry(width=10, font=(FONT_NAME, 20))
+# Spinbox for Number of Letters
+letter_input = Spinbox(from_=0, to=20, width=10, font=(FONT_NAME, 20))
 letter_input.place(x=675, y=100)
 
+# Label for Number of Symbols
 symbols_label = Label(text="2. How many symbols would you like?", bg=YELLOW, fg=BLACK, font=(FONT_NAME, 20))
 symbols_label.place(x=10, y=150)
 
-symbols_input = Entry(width=10, font=(FONT_NAME, 20))
+# Spinbox for Number of Symbols
+symbols_input = Spinbox(from_=0, to=20, width=10, font=(FONT_NAME, 20))
 symbols_input.place(x=675, y=150)
 
+# Label for Number of Numbers
 number_label = Label(text="3. How many numbers would you like?", bg=YELLOW, fg=BLACK, font=(FONT_NAME, 20))
 number_label.place(x=10, y=200)
 
-number_input = Entry(width=10, font=(FONT_NAME, 20))
+# Spinbox for Number of Numbers
+number_input = Spinbox(from_=0, to=20, width=10, font=(FONT_NAME, 20))
 number_input.place(x=675, y=200)
 
+# Button for Generating Password
 generate_button = Button(text="Generate", bg=PURPLE, font=(FONT_NAME, 20), command=generate_password)
 generate_button.pack(pady=200)
 
-password_field = Entry(width=42, font=(FONT_NAME, 30), fg=RED, state="disabled")
+# Field for the Generated Password
+password_field = Entry(width=42, font=(FONT_NAME, 30), fg=RED)
 password_field.place(x=10, y=350)
 
 # Screen configurations
