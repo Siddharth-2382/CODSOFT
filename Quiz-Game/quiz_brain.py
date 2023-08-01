e74e4fa7de8436c23765f458ec6f92bbd503111f
Siddharth Movaliya <siddraj1992@gmail.com>
2023-08-01 15:01:11 +0530

    add logic for the quiz brain which formats and prepares quizz questions

---
+++ Quiz-Game/quiz_brain.py
@@ -0,0 +1,32 @@
+import html
+
+
+# Main Quiz Brain logic
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+        self.current_question = None
+
+    # Check if there are any more questions left
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    # Method to get the next question
+    def next_question(self):
+        self.current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        # Format the data and unescape html formats
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
+
+    # Check if the answer selected by user is correct
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            return True
+        else:
+            return False
