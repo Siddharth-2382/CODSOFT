c7d6c4180fcbf593315b1c0a3ec64ca1ef8ecfac
Siddharth Movaliya <siddraj1992@gmail.com>
2023-08-03 16:02:43 +0530

    add functionality to button press for input field

--- Calculator-App/main.py
+++ Calculator-App/main.py
@@ -7,7 +7,7 @@ FONT_NAME = "Courier"
 
 # Function to handle button press for expression values
 def press(x):
-    pass
+    input_entry.insert(END, str(x))
 
 
 def clear():
@@ -25,56 +25,56 @@ input_entry = Entry(font=(FONT_NAME, 25))
 input_entry.grid(row=0, column=0, columnspan=4, pady=5, padx=5)
 
 # Buttons for numbers
-b0 = Button(text="0", height=1, width=2, font=(FONT_NAME, 25), command=press(0))
+b0 = Button(text="0", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(0))
 b0.grid(row=4, column=0, pady=15)
 
-b1 = Button(text="1", height=1, width=2, font=(FONT_NAME, 25), command=press(1))
+b1 = Button(text="1", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(1))
 b1.grid(row=3, column=0, pady=15)
 
-b2 = Button(text="2", height=1, width=2, font=(FONT_NAME, 25), command=press(2))
+b2 = Button(text="2", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(2))
 b2.grid(row=3, column=1, pady=15)
 
-b3 = Button(text="3", height=1, width=2, font=(FONT_NAME, 25), command=press(3))
+b3 = Button(text="3", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(3))
 b3.grid(row=3, column=2, pady=15)
 
-b4 = Button(text="4", height=1, width=2, font=(FONT_NAME, 25), command=press(4))
+b4 = Button(text="4", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(4))
 b4.grid(row=2, column=0, pady=15)
 
-b5 = Button(text="5", height=1, width=2, font=(FONT_NAME, 25), command=press(5))
+b5 = Button(text="5", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(5))
 b5.grid(row=2, column=1, pady=15)
 
-b6 = Button(text="6", height=1, width=2, font=(FONT_NAME, 25), command=press(6))
+b6 = Button(text="6", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(6))
 b6.grid(row=2, column=2, pady=15)
 
-b7 = Button(text="7", height=1, width=2, font=(FONT_NAME, 25), command=press(7))
+b7 = Button(text="7", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(7))
 b7.grid(row=1, column=0, pady=15)
 
-b8 = Button(text="8", height=1, width=2, font=(FONT_NAME, 25), command=press(8))
+b8 = Button(text="8", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(8))
 b8.grid(row=1, column=1, pady=15)
 
-b9 = Button(text="9", height=1, width=2, font=(FONT_NAME, 25), command=press(9))
+b9 = Button(text="9", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press(9))
 b9.grid(row=1, column=2, pady=15)
 
 # Buttons for different functions
 clear_btn = Button(text="CLEAR", height=1, width=18, font=(FONT_NAME, 25), command=clear)
 clear_btn.grid(row=5, column=0, columnspan=4, pady=15)
 
-divide = Button(text="/", height=1, width=2, font=(FONT_NAME, 25), command=press("/"))
+divide = Button(text="/", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press("/"))
 divide.grid(row=1, column=3)
 
-multiply = Button(text="*", height=1, width=2, font=(FONT_NAME, 25), command=press("*"))
+multiply = Button(text="*", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press("*"))
 multiply.grid(row=2, column=3)
 
-addition = Button(text="+", height=1, width=2, font=(FONT_NAME, 25), command=press("+"))
+addition = Button(text="+", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press("+"))
 addition.grid(row=3, column=3)
 
-subtract = Button(text="-", height=1, width=2, font=(FONT_NAME, 25), command=press("-"))
+subtract = Button(text="-", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press("-"))
 subtract.grid(row=4, column=3)
 
 equal = Button(text="=", height=1, width=2, font=(FONT_NAME, 25), command=evaluate)
 equal.grid(row=4, column=2)
 
-point = Button(text=".", height=1, width=2, font=(FONT_NAME, 25), command=press("."))
+point = Button(text=".", height=1, width=2, font=(FONT_NAME, 25), command=lambda: press("."))
 point.grid(row=4, column=1)
 
 # Window configuration
